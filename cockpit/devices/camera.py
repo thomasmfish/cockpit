thomasmfish/cockpit

commit f1e94bc4207190dcdf177b8bcd85a3be17a7c424

Fix/cleanup settings code. Add TupleOfInts setting.

cockpit/gui/device.py
@@ -272,23 +272,42 @@ class EnableButton(wx.ToggleButton):
         self.setState(state)
 
 
+class TupleOfIntsProperty(wx.propgrid.StringProperty):
+    def __init__(self, *args, **kwargs):
+        if 'value' in kwargs:
+            kwargs['value'] = ", ".join([str(v) for v in kwargs['value']])
+        super().__init__(*args, **kwargs)
+
+
+    def SetValue(self, value, **kwargs):
+        value = ", ".join([str(v) for v in value])
+        super().SetValue(value, **kwargs)
+
+
+    def GetValue(self):
+        return tuple([int(v) for v in self.m_value.split(",")])
+
+
 class SettingsEditor(wx.Frame):
     _SETTINGS_TO_PROPTYPES = {'int': wx.propgrid.IntProperty,
                              'float': wx.propgrid.FloatProperty,
                              'bool': wx.propgrid.BoolProperty,
                              'enum': wx.propgrid.EnumProperty,
                              'str': wx.propgrid.StringProperty,
+                             'tuple': TupleOfIntsProperty,
                              str(int): wx.propgrid.IntProperty,
                              str(float): wx.propgrid.FloatProperty,
                              str(bool): wx.propgrid.BoolProperty,
-                             str(str): wx.propgrid.StringProperty, }
+                             str(str): wx.propgrid.StringProperty,
+                             str(tuple): TupleOfIntsProperty}
 
 
     def __init__(self, device, parent=None, handler=None):
         wx.Frame.__init__(self, parent, wx.ID_ANY, style=wx.FRAME_FLOAT_ON_PARENT)
         self.device = device
         self.SetTitle("%s settings" % device.name)
-        self.settings = None
+        self.settings = {}
+        self.current = {}
         self.handler = handler
         #self.handler.addListener(self)
         #self.panel = wx.Panel(self, wx.ID_ANY, style=wx.WANTS_CHARS)
@@ -329,11 +348,7 @@ class SettingsEditor(wx.Frame):
         self.SetMinSize((256, -1))
         #self.SetMaxSize((self.GetMinWidth(), -1))
         events.subscribe("%s settings changed" % self.device, self.updateGrid)
-
-
-    def onEnabledEvent(self, evt):
-        if self.IsShown():
-            self.updateGrid()
+        self.Bind(wx.EVT_SHOW, lambda evt: self.updateGrid())
 
 
     def onClose(self, evt):
@@ -347,25 +362,17 @@ class SettingsEditor(wx.Frame):
         prop = event.GetProperty()
         name = event.GetPropertyName()
         setting = self.settings[name]
-        # Fetch and validate the value.
-        if prop.ClassName == 'wxEnumProperty':
-            index = event.GetPropertyValue()
-            # Look up value as the original type, not as str from the wxProperty.
-            # setting['values'] only contains allowed values, so this also
-            # serves as validation for enums.
-            value = setting['values'][index]
-        elif setting['type'] in (str(int), str(float), 'int', 'float'):
-            value = event.GetPropertyValue()
+        # Fetch and validate the value from the control - using event.GetValue
+        # may return the wrong type for custom properties.
+        value = prop.GetValue()
+
+        if setting['type'] in (str(int), str(float), 'int', 'float'):
             # Bound to min/max.
             lims = setting['values']
-            value = sorted(lims + (value,))[1]
+            value = sorted(tuple(lims) + (value,))[1]
         elif setting['type'] in (str(str), 'str'):
             # Limit string length.
             value = value[0, setting['values']]
-        elif setting['type'] in (str(bool), 'bool'):
-            value = event.GetPropertyValue()
-        else:
-            raise Exception('Unsupported type.')
 
         self.current[name] = value
         if value != self.device.settings[name]:
@@ -385,77 +392,63 @@ class SettingsEditor(wx.Frame):
 
 
     def updateGrid(self):
+        """Update property state and values.
+
+        Note: grid.SetValues does not work for custom property classes -
+        it seems that it calls the C++ SetValue on the base class rather
+        than the python SetValue on the derived class."""
         if not self.IsShown():
             return
         self.Freeze()
         grid = self.grid
         self.settings = OrderedDict(self.device.describe_settings())
-        self.current.update(self.device.settings)
-        # Update all values.
-        # grid.SetValues(current)
-        # Enable/disable
+        if self.current:
+            self.current.update(self.device.settings)
+        else:
+            self.current = self.device.get_all_settings()
+        # Enable/disable controls, and update Choices for enums.
         for prop in grid.Properties:
             prop.SetTextColour(wx.Colour(0, 0, 0))
             name = prop.GetName()
             desc = self.settings[name]
             if desc['type'] in ('enum'):
-                if version.LooseVersion(wx.__version__) < version.LooseVersion('4'):
-                    choices = wx.propgrid.PGChoices()
-                    for i, d in enumerate(desc['values']):
-                        choices.Add(str(d), i)
-                else:
-                    choices = wx.propgrid.PGChoices([str(v) for v in desc['values']],
-                                                    range(len(desc['values'])))
+                indices, items = zip(*desc['values'])
+                labels = [str(i) for i in items]
+                choices = wx.propgrid.PGChoices(labels, indices)
                 prop.SetChoices(choices)
-                if self.current[name] in desc['values']:
-                    index = desc['values'].index(self.current[name])
-                    prop.SetValue(index)
-                else:
+                if self.current[name] not in indices:
                     # Indicate a problem with this item.
                     prop.SetTextColour('red')
-            else:
-                value = self.current[name]
-                prop.SetValue(value)
             try:
                 prop.Enable(not self.settings[name]['readonly'])
             except wx._core.PyAssertionError:
                 # Bug in wx in stc.EnsureCaretVisible, could not convert to a long.
                 pass
+            prop.SetValue(self.current[name])
         self.Thaw()
 
 
     def populateGrid(self):
+        """Create the propertgrid controls.
+
+        We just create the controls here - their values will be updated
+        by updateGrid."""
         grid = self.grid
         self.settings = OrderedDict(self.device.describe_settings())
-        self.current = self.device.get_all_settings()
         for key, desc in iteritems(self.settings):
-            value = self.current[key]
-            # For some reason, a TypeError is thrown on creation of prop if value
-            # is a zero-length string.
-            if value == '':
-                value  = ' '
             propType = SettingsEditor._SETTINGS_TO_PROPTYPES.get(desc['type'])
-            if propType is wx.propgrid.EnumProperty:
-                if value in desc['values']:
-                    index = desc['values'].index(value)
-                    prop = wx.propgrid.EnumProperty(label=key, name=key,
-                                                    labels=[str(v) for v in desc['values']],
-                                                    values=range(len(desc['values'])),
-                                                    value=index)
-                else:
-                    prop = wx.propgrid.EnumProperty(label=key, name=key,
-                                                    labels=[str(v) for v in desc['values']],
-                                                    values=range(len(desc['values'])))
-            else:
-                try:
-                    prop = propType(label=key, name=key, value=(value or 0))
-                except OverflowError:
-                    # Int too large.
-                    prop = wx.propgrid.FloatProperty(label=key, name=key, value=str(value or 0))
-                except Exception as e:
-                    sys.stderr.write("populateGrid threw exception for key %s with value %s: %s"
-                                     % (key, value, e.message))
-
+            if propType is wx.propgrid.IntProperty:
+                # Use a float if integer may exceed IntProperty representation.
+                # The representation is dependent on whether or not  wx was compiled
+                # with wxUSE_LONG_LONG defined. I can't find a way to easily figure
+                # this out from python, so we go for the safer limit.
+                if max(desc['values']) > wx.INT32_MAX:
+                    propType = wx.propgrid.FloatProperty
+            try:
+                prop = propType(label=key, name=key)
+            except Exception as e:
+                sys.stderr.write("populateGrid threw exception for key %s with value %s: %s"
+                                 % (key, value, e))
             if desc['readonly']:
                 prop.Enable(False)
             grid.Append(prop)

commit 0f8fce724e8ab4a7861ac01f17baa1e3a35c1499

Squashed commit of the following:
commit bffc2aa87b25304da8c3af03ed7b8920dd15cd7c
Author: David Miguel Susano Pinto <carandraug+dev@gmail.com>
Date:   Thu Nov 10 19:05:36 2022 +0000

    SettingsEditor.populateGrid: remove try/except around creating property value.

    The handling of this exception has been broken since f1e94bc42 (almost
    four years ago) because the `value` variable used in the except block
    no longer exists.

    This exception handling was probably meant to catch exceptions when a
    value was invalid for a property.  However, the properties are now
    constructed without a value and updated later so this is no longer
    needed.  In addition, even if an exception was caught, it didn't
    `continue` to the next property so it would fail when appending to the
    grid of windows.

commit d1c8bcd21e894fdbd04a374a45c65823566b75f4
Author: Thomas Fish <thomas.fish@diamond.ac.uk>
Date:   Thu Nov 10 17:31:48 2022 +0000

    ChannelsPanel.GetButtonByLabel: fix wrong variable name (fixup 72c9f9f8c)

commit 6c507b82ccd17fe961b20b30178f23373e0d49cf
Author: Thomas Fish <thomas.fish@diamond.ac.uk>
Date:   Thu Nov 10 17:36:28 2022 +0000

    cockpit/util/Mrc.py: replace `raw_input` (Python 2 only) with `input`.

commit 2d4c9492bbce4106620470c458fc238c698629d6
Author: Thomas Fish <thomas.fish@diamond.ac.uk>
Date:   Thu Nov 10 18:01:11 2022 +0000

    cockpit/devices/camera.py: fix typo on a comment.

commit c12606ad68fc6f47dc13422d48f27579c22bb1c2
Author: Thomas Fish <thomas.fish@diamond.ac.uk>
Date:   Thu Nov 10 17:31:10 2022 +0000

    PolarizationDevice: remove shadowed and outdated getHandlers.

    b779b7e017bf introduced a new implementation of getHandlers but did
    not remove the previous one.  Later, 2d7e016a4d5f replaced that
    previous one but left the second around which is now shadowed.  This
    commit removes that second unreachable implementation.

commit b2f02e447449b75ed9e065fdc349c82c52d30283

cockpit/devices/camera.py
@@ -84,7 +84,7 @@ class CameraDevice(device.Device):
         self._setTransform((lr, ud, rot))
 
     def _setTransform(self, transform):
-        # Sublcasses should override this if transforms are done on the device.
+        # Subclasses should override this if transforms are done on the device.
         self._transform = transform
 
     def finalizeInitialization(self):

cockpit/devices/polarizationRotator.py
@@ -27,7 +27,7 @@ class PolarizationDevice(device.Device):
 
     The device class makes available a handler for SI experiments
     which takes an integer argument in the action table that specifies
-    the SI angle index.  Upon exmining the table, it replaces
+    the SI angle index.  Upon examining the table, it replaces
     instances of this handler with instances of whatever handler
     drives the analogue out- put, having converted the angle index to
     the required voltage.
@@ -66,27 +66,6 @@ class PolarizationDevice(device.Device):
 
     def __init__(self, name, config={}):
         super().__init__(name, config)
-
-
-    def getHandlers(self):
-        asource = self.config.get('analogsource', None)
-        aline = self.config.get('analogline', None)
-        offset = self.config.get('offset', 0)
-        gain = self.config.get('gain', 1)
-        dt = Decimal(self.config.get('settlingtime', 0.05))
-        aHandler = depot.getHandler(asource, depot.EXECUTOR)
-
-        if aHandler is None:
-            raise Exception('No control source.')
-        movementTimeFunc = lambda x, start, delta: (0, dt)
-        handler = aHandler.registerAnalog(self, aline, offset, gain, movementTimeFunc)
-
-        # Connect handler to analogue source to populate movement callbacks.
-        handler.connectToAnalogSource(aHandler, aline, offset, gain)
-
-        result.append(handler)
-        return result
-
 
     def getHandlers(self):
         aSource = self.config.get('analogsource', None)

cockpit/gui/device.py
@@ -457,11 +457,7 @@ class SettingsEditor(wx.Frame):
                 # the max() comparison in that case.
                 if None in desc['values'] or max(desc['values']) > wx.INT32_MAX:
                     propType = wx.propgrid.FloatProperty
-            try:
-                prop = propType(label=key, name=key)
-            except Exception as e:
-                sys.stderr.write("populateGrid threw exception for key %s with value %s: %s"
-                                 % (key, value, e))
+            prop = propType(label=key, name=key)
             if desc['readonly']:
                 prop.Enable(False)
             grid.Append(prop)

cockpit/gui/mainPanels.py
@@ -301,7 +301,7 @@ class ChannelsPanel(wx.Panel):
                 return sizer_item.Window
         else:
             raise ValueError('There is no button named \'%s\''
-                             % channel_name)
+                             % name)
 
 
     def OnChannelAdded(self, event: wx.CommandEvent) -> None:

cockpit/interfaces/stageMover.py
@@ -58,7 +58,6 @@ from cockpit import depot
 from cockpit import events
 from cockpit.util import userConfig
 from cockpit.util.exceptions import MotionError
-import cockpit.util.threads
 
 import numpy
 import threading

cockpit/util/Mrc.py
@@ -322,7 +322,7 @@ def save(a, fn, ifExists='ask', zAxisOrder=None,
         if ifExists[0] == 'o':
             pass
         elif ifExists[0] == 'a':
-            yes = raw_input("overwrite?").lower() == 'y'
+            yes = input("overwrite?").lower() == 'y'
             if not yes:
                 raise RuntimeError("not overwriting existing file '%s'"%fn)
         else: