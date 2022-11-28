18a2e1617a6ab810a3fcc5aa50e36c7651763b60
Thomas Fish <thomas.fish@diamond.ac.uk>
2022-11-28 17:27:44 +0000

    allow directories and files to be specified

--- tools/reorder_SIM_experiment_data.py
+++ tools/reorder_SIM_experiment_data.py
@@ -1,5 +1,7 @@
 import dataclasses
 import sys
+import os.path
+from glob import glob
 
 from cockpit.experiment import structuredIllumination
 from cockpit.util.datadoc import DataDoc
@@ -14,10 +16,24 @@ class FakeSIExperiment:
     numPhases: int
 
 
-for fpath in sys.argv[1:]:
-    doc = DataDoc(fpath)
-    n_z = int(doc.getNPlanes() / 2 / 3 /5) # 2 channels, 3 angles, 5 phases
-    del doc # workaround bug in datadoc
-    experiment = FakeSIExperiment(fpath, "Z, Angle, Phase", 3, n_z, 5)
-    print('doing ', fpath)
-    structuredIllumination.SIExperiment.reorder_img_file(experiment)
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    dirs = set(d for d in args if os.path.isdir(d))
+    files = set(p for p in args if os.path.isfile(p))
+    if dirs:
+        for d in dirs:
+            files.update([p for p in glob(os.path.join(d, "*")) if os.path.isfile(p)])
+
+    for fpath in files:
+        doc = DataDoc(fpath)
+        n_z = int(doc.getNPlanes() / 2 / 3 /5) # 2 channels, 3 angles, 5 phases
+        del doc # workaround bug in datadoc
+        experiment = FakeSIExperiment(fpath, "Z, Angle, Phase", 3, n_z, 5)
+        try:
+            print('doing ', fpath)
+            structuredIllumination.SIExperiment.reorder_img_file(experiment)
+        except Exception:
+            print("Processing for %s failed" % fpath)
+        except:
+            print("Processing was stopped midway through %s" % fpath)
+            break
